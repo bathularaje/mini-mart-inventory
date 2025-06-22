867996db567d81fe66d371531150d99e0bebd953
bathularaje <167248706+bathularaje@users.noreply.github.com>
2025-06-22 17:31:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
