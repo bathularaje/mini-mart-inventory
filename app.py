83bf75f15bef3e380b7e39d5a7a7a6bdb9f2f466
bathularaje <167248706+bathularaje@users.noreply.github.com>
2025-07-13 02:23:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -122,8 +122,6 @@ def delete_product(product_id):
     db.session.commit()
     return jsonify({'message': 'Product deleted'})
 
-
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
